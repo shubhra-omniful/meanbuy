7439dab1f89bcfcd9d4ac5cf8959351975a0fb57
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-06-16 17:23:19 +0530

    ++

---
+++ login/login.js
@@ -0,0 +1,31 @@
+let form=document.querySelector("form");
+let userData=JSON.parse(localStorage.getItem("userData"));
+    
+form.addEventListener("submit",function(event){
+        event.preventDefault();
+
+    let data={
+          email:form.email.value,
+          password:form.password.value,
+    };    
+
+        if(userData ==null)
+        {
+            alert("please create an account");
+        }
+        else if(userData.email===data.email && userData.pass1===data.password)
+        {
+            alert("sign in successful");
+
+            localStorage.setItem("signin",JSON.stringify(userData)); 
+    // redirect to homepage
+         window.location.href="index.html";      
+        }
+        else{
+            alert("invalid email or password");
+        }
+    });
+    
+import {header} from "./component/navbar.js";
+let navbar= document.getElementById('header');
+navbar.innerHTML=header();
---
+++ login/signup.js
@@ -0,0 +1,24 @@
+let form=document.getElementById('form');
+form.addEventListener('submit',function(){
+    event.preventDefault();
+
+    let data={
+       email:form.email.value,
+       pass1:form.pass1.value,
+       pass2:form.pass2.value,   
+    }
+
+    if(data.pass1==data.pass2)
+    {
+        localStorage.setItem('userData',JSON.stringify(data));
+        window.location.reload();
+    }
+    else{
+        alert('password not matched');
+    }
+        
+})
+
+import {header} from "./component/navbar.js";
+let navbar= document.getElementById('header');
+navbar.innerHTML=header();
